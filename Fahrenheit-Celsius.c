0a083db70dd3f51dcea3160d7f86e0cfdc704e65
Abigail Glime <20glime@stfrancishs.org>
2018-09-07 19:51:37 +0000

    init

---
+++ Fahrenheit-Celsius.c
@@ -0,0 +1,16 @@
+/* Fahernheit - Celsius */
+
+#include <stdio.h>
+
+int main(void)
+
+{
+    int fahr;
+
+    for (fahr = 0; fahr <= 300; fahr = fahr +20)
+
+{
+    printf("f- %3d c- %6.1f\n" ,fahr, (5.0/9.0)*(fahr-32));
+
+}
+}
